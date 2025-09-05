ab3203cf1ba6dfe9c78563b09ecd8fb064055e59
PradeepTmg <tamangpradeep88@gmail.com>
2025-09-06 03:23:04 +0545

    Set DEBUG=False for Render deployment

--- birthday_site/birthday_site/settings.py
+++ birthday_site/birthday_site/settings.py
@@ -37,12 +37,16 @@ ALLOWED_HOSTS = [
 
 
 
-DEBUG = True
+DEBUG = False
+
+DEBUG = os.environ.get('RENDER') is None
+
 
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Application definition
@@ -133,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
