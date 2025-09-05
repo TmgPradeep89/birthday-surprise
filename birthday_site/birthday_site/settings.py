5fc02d900aa730d701e92926f0a783d6cf21d999
PradeepTmg <tamangpradeep88@gmail.com>
2025-09-06 02:04:29 +0545

    Fix ALLOWED_HOSTS to read from environment

--- birthday_site/birthday_site/settings.py
+++ birthday_site/birthday_site/settings.py
@@ -23,8 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7w5si-zb@u$(md)$xgl41!n6**^78jf^#2vrb(k8%5ziex#pr#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
-print("ALLOWED_HOSTS from env:", ALLOWED_HOSTS)
+import os
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
 
 
 DEBUG = True
